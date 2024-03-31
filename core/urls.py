185fb133d92bbdbabb3b939a508cde55429b9e22
matheus-feu <matheusfeu@gmail.com>
2024-03-31 12:30:15 -0300

    update static files

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -12,12 +14,12 @@ from products.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_auth_token),
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/create/', ProductCreateView.as_view(), name='product-create'),
-    path('products_detail/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
-    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
-    path('products/reactivate/<int:pk>/', ProductReactivateView.as_view(), name='product-reactivate'),
-]
+                  path('admin/', admin.site.urls),
+                  path('api-token-auth/', obtain_auth_token),
+                  path('products/', ProductListView.as_view(), name='product-list'),
+                  path('products/create/', ProductCreateView.as_view(), name='product-create'),
+                  path('products_detail/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+                  path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
+                  path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
+                  path('products/reactivate/<int:pk>/', ProductReactivateView.as_view(), name='product-reactivate'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
